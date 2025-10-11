47cf4c3ec64c52c56da282117fc9f9b42352e790
aigul <aigulsobakuno@gmail.com>
2025-10-11 10:59:20 +0300

    delet

--- test.c
+++ test.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-
-int main() {
-    printf("Hello! From 1Dev!");
-    //версия из main
-    printf("Bye!");
-    // версия из develop2
+int main() { 
+    printf("Test 1\n");
+    return 0; 
 }
